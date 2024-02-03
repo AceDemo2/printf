ac53cddc4f4cb94f203a521b897bc18eccb674e0
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-03 09:04:49 -0800

    to handle c,s,%

--- handle_cs%.c
+++ handle_cs%.c
@@ -1,46 +1,56 @@
 #include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
-{       
+{
 	char *j;
 	char l = '%';
-        int k, m = 0;
+	int k, m = 0;
 	va_list i;
+
 	if (format == NULL)
 		return (-1);
+
 	va_start(i, format);
+
 	while (*format)
-        {
+	{
 		if (*format == '%' && *(format + 1) != '\0')
 		{
 			switch (*(format + 1))
 			{
-				case 'c':
-                                	k = va_arg(i, int);
-					write(1, &k, 1);
-					m++;
-					format += 2;
-					break;
-				case 's':
-					j = va_arg(i, char *);
-					if (j == NULL)
-						j = "(null)";
-					write(1, j, strlen(j));
-					m += strlen(j);
-					format += 2;
-					break;
-				case '%':
-					write(1, &l, 1);
-					m++;
-					format += 2;
-					break;
-				default:
-					write(1, (format), 1);
-					m++;
-					format += 1;
-					break;
+			case 'c':
+				k = va_arg(i, int);
+				write(1, &k, 1);
+				m++;
+				format += 2;
+				break;
+			case 's':
+				j = va_arg(i, char *);
+				if (j == NULL)
+					j = "(null)";
+				write(1, j, strlen(j));
+				m += strlen(j);
+				format += 2;
+				break;
+			case '%':
+				write(1, &l, 1);
+				m++;
+				format += 2;
+				break;
+			default:
+				write(1, format, 1);
+				m++;
+				format += 1;
+				break;
 			}
 		}
-		else if (*(format) == '%' && *(format + 1) == '\0')
+		else if (*format == '%' && *(format + 1) == '\0')
 			return (-1);
 		else
 		{
@@ -49,6 +59,8 @@ int _printf(const char *format, ...)
 			format++;
 		}
 	}
+
 	va_end(i);
 	return (m);
 }
+
