9dc19799778356b1cde00c50558d4ef6cbbe8b38
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-03 13:14:40 -0800

    to handle c,s,%

--- handle_cs%.c
+++ handle_cs%.c
@@ -20,22 +20,21 @@ int _printf(const char *format, ...)
 			if (*(format + 1) == 'c')
 			{
 				k = va_arg(i, int);
-				write(1, &k, 1);
+				m += write(1, &k, 1);
 			}
 			else if (*(format + 1) == 's')
 			{
 				j = va_arg(i, char *);
 				j = (j == NULL) ? "(null)" : j;
-				write(1, j, strlen(j));
+				m += write(1, j, strlen(j));
 			}
 			else if (*(format + 1) == '%')
-				write(1, &l, 1);
+				m += write(1, &l, 1);
 			else
 			{
-				write(1, format, 1);
+				m += write(1, format, 1);
 				n = 1;
 			}
-			m = (*(format + 1) != 's') ? m + 1 : m + (int)strlen(j);
 			format = (n == 1) ? format + 1 : format + 2;
 		}
 		else
