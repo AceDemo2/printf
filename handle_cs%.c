b217909386c66b09560da92f4c6e8c7d1c2aea4a
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-02 17:32:39 -0800

    to handle c,s,%

--- handle_cs%.c
+++ handle_cs%.c
@@ -3,13 +3,13 @@
 int _printf(const char *format, ...)
 {
     va_list i;
-    if (format == NULL)
-        return 0;
 
     char *j;
     char l = '%';
     int k, m = 0;
 
+    if (format == NULL)
+        return 0;
     va_start(i, format);
 
     while (*format)
