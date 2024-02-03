3e6441a1447e71fd4d1f362379125a76e0b56383
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-02 17:58:54 -0800

    to handle c,s,%

--- handle_cs%.c
+++ handle_cs%.c
@@ -1,47 +1,56 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
-int _printf(const char *format, ...) {
-    int count = 0;
-    char c;
-    const char *str;
-
-    va_list args;
-    va_start(args, format);
-
-    while (format && *format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    c = (char)va_arg(args, int);
-                    count += write(1, &c, 1);
-                    break;
-                case 's':
-                    str = va_arg(args, const char *);
-                    if (str) {
-                        while (*str) {
-                            count += write(1, str, 1);
-                            str++;
-                        }
-                    }
-                    break;
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-                default:
-                    count += write(1, format - 1, 1);
-                    break;
-            }
-        } else {
-            count += write(1, format, 1);
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
+int _printf(const char *format, ...)
+{       
+	char *j;
+	char l = '%';
+        int k, m = 0;
+	va_list i;
+	if (format == NULL)
+		return (0);
+	va_start(i, format);
+	while (*format)
+        {
+		if (*format == '%' && *(format + 1))
+		{
+			switch (*(format + 1))
+			{
+				case 'c':
+                                	k = va_arg(i, int);
+					write(1, &k, 1);
+					m++;
+					format += 2;
+					break;
+				case 's':
+					j = va_arg(i, char *);
+					if (j == NULL)
+						j = "(null)";
+					write(1, j, strlen(j));
+					m += strlen(j);
+					format += 2;
+					break;
+				case '%':
+					write(1, &l, 1);
+					m++;
+					format += 2;
+					break;
+				default:
+					write(1, (format), 1);
+					format += 1;
+					break;
+			}
+		}
+		else if (*format == '%' && !(*(format + 1)))
+		{
+			write(1, format, 1);
+			return (0);
+		}
+		else
+		{
+			write(1, format, 1);
+			m++;
+			format++;
+		}
+	}
+	va_end(i);
+	return (m);
 }
-
