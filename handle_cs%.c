cccee3949ee17d4ab1ec5f2f892cd13267bc3e1e
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-02 16:06:05 -0800

    to handle c,s,%

---
+++ handle_cs%.c
@@ -0,0 +1,32 @@
+#include "header.h"
+int _printf(const char *format, ...)
+{       
+	char *j;
+        va_list i;
+        va_start(i, format);
+        while (*format)
+        {
+                if (*format == %)
+		{
+			switch (*(format + 1))
+			{
+                        	case 'c':
+                                	j = va_arg(i, int);
+					write(1, j, 1);
+					break;
+				case 's':
+					j = va_arg(i, char *);
+					write(1, j, strlen(j));
+					break;
+				case '%':
+					write(1, %, 1);
+					break;
+				default:
+					break;
+			}
+		}
+		else
+			write(1, format, 1);
+		format++;
+	}
+}
---
+++ header.h
@@ -0,0 +1,7 @@
+#ifndef MAIN
+#define MAIN
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+int _printf(const char *format, ...);
