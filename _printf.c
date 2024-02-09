853fb4a916471d843136978667583f1d27452079
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-09 14:09:01 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
 	int m = 0, n = 0;
