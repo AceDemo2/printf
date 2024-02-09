e1ee536a3b0736a0811f6ed8c628ad9b57bd3190
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-02-09 21:01:40 +0100

    Create handle_di.c

---
+++ handle_di.c
@@ -0,0 +1,43 @@
+int handle_id(long n)
+{
+  long int l = 0, m = 0, j, o, n;
+	char *k;
+          format += 2;
+					j = va_arg(i, int);
+					if (j < 0)
+					{
+						m += write(1, "-", 1);
+						j = -j;
+					}
+					if (j == 0)
+						l++;
+					else
+					{
+						o = j;
+						while (o != 0)
+						{
+							o = o / 10;
+							l++;
+						}
+					}
+					k = malloc(sizeof(char) * l + 1);
+					if (k == NULL)
+					{
+						va_end(i);
+						return (-1);
+					}
+					k[l] = '\0';
+					n = l - 1;
+					while (n >= 0)
+					{
+						k[n] = j % 10 + '0';
+						m++;
+						n--;
+						j /= 10;
+					}
+					write(1, k, strlen(k));
+					free(k);
+  l = 0;
+  return (m);
+}
+					
