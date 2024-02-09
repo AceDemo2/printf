7647f822ee20b9f9c9a07f7d4a11b3eca6f604a9
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-09 15:08:30 -0800

    handle_b

--- b_handle.c
+++ b_handle.c
@@ -4,26 +4,31 @@ int handle_b(unsigned int b)
 	unsigned int l = 0, k, m = 0;
 	char *j;
 	k = b;
-	while (b != 0)
+	if (b == 0)
+		write(1, "0", 1);
+	else
 	{
-		b /= 2;
-		l++;
+		while (b != 0)
+		{
+			b /= 2;
+			l++;
+		}
+		j = malloc(sizeof(unsigned int) * l);
+		if (j == NULL)
+		{
+			return(-1);
+		}
+		j[l] = '\0';
+		while(l > 0)
+		{
+			l--;
+			j[l] = (k % 2) + '0';
+			k /= 2;
+			m++;
+		}
+		write(1, j, strlen(j));
+		free(j);
+		l = 0;
 	}
-	j = malloc(sizeof(unsigned int) * l + 1);
-	if (j == NULL)
-	{
-		return(-1);
-	}
-	j[l] = '\0';
-	while(l > 0)
-	{
-		l--;
-		j[l] = (k % 2) + '0';
-		k /= 2;
-		m++;
-	}
-	write(1, j, strlen(j));
-	free(j);
-	l = 0;
 	return (m);
 }
