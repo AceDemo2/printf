e4853143c7c2da2c7873b65e728e345979841de8
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-10 09:41:59 -0800

    printf

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int _printf(const char *format, ...);
 int handle_id(long j);
 int handle_c(int c);
 int handle_s(char *j);
-int custom_specifier(char *c)
+int custom_specifier(char *c);
 int handle(unsigned int b, int c, int a);
 void dbuff(int a, char *b);
 #endif
