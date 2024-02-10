dc36fef10f2b793e37d479063beaa5b4c970cbee
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-10 12:46:44 -0800

    address

--- handle_p.c
+++ handle_p.c
@@ -1,9 +1,9 @@
 #include "main.h"
+#include <stdint.h>
 int handle_p(void *p)
 {
 	int m = 0;
-	unsigned int a = (unsigned int)p;
-
+	uintptr_t a = (uintptr_t)p;
 	m += handle(a, 16, 2);
 	return (m);
 }
