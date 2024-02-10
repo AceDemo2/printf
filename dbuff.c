a39938eda9a84f65c501e6bdd887b86c9fdee5c4
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-10 10:43:56 -0800

    printf

--- dbuff.c
+++ dbuff.c
@@ -1,11 +1,13 @@
 #include "main.h"
 void dbuff(int a, char *b)
 {
-	int i = 1;
+	int i = 1, d;
+	char *c = "0123456789ABCDEF";
 
 	while (a != 0)
 	{
-		b[i] = a % 16;
+		d = a % 16;
+		b[i] = c[d];
 		a /= 16;
 		i--;
 	}
