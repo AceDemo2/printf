032a8c535617a47d61d48557a141f44b80d8f6c8
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-10 13:20:46 -0800

    address

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ int handle_id(long j);
 int handle_c(int c);
 int handle_s(char *j);
 int custom_specifier(char *c);
-int handle(unsigned long b, int c, int a);
+int handle(unsigned int b, int c, int a);
 void dbuff(int a, char *b);
 int handle_p(void * p);
 #endif
