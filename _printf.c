e377579d8e1dbbc8157d46d4de9078a58f6ea6fa
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-12 15:09:45 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -34,11 +34,6 @@ int _printf(const char *format, ...)
 							format++;
 							l = 0;
 						}
-						else if (j <= 0)
-						{
-							format++;
-							l = 0;
-						}
 						else
 						{
 							format++;
