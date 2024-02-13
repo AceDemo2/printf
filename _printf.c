bb25e1a3786050d64d15349bb00e60cb053a032a
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-13 11:07:22 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
                                         o = 1;
                                         if (*format == '+')
                                         {
-						if (j && j >= 0 && n == 0)
+						if (j >= 0 && n == 0)
                                                 {
                                                         m += write(1, format, 1);
                                                         format++;
@@ -43,7 +43,7 @@ int _printf(const char *format, ...)
                                                                                         }
                                         else if (*format == ' ')
                                         {
-                                         	if (j && j >= 0 && n == 0 && q == 0)
+                                         	if (j >= 0 && n == 0 && q == 0)
                                                 {
                                                         m += write(1, format, 1);
                                                         format++;
