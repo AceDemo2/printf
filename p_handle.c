1a220c697ed0bd7437f1cbd2ffd4361a26f395d6
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-02-14 17:34:25 +0100

    Update p_handle.c

--- p_handle.c
+++ p_handle.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdint.h>
-int handle_p(void *p, int w)
+int handle_p(va_list i, int w)
 {
 	int m = 0;
 
 	if (p == NULL)
 		m += write(1, "(nil)", 5);
 	else
-		m += handle((unsigned long)p, 16, 2, 0, 0, w);
+		m += handle((i, 16, 2, 0, 0, w);
 	return (m);
 }
