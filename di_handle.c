5c65e9a642b985a5ef0e4c43b58974e19aeb0084
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-14 09:57:26 -0800

    Update convert_base.c

--- di_handle.c
+++ di_handle.c
@@ -13,6 +13,10 @@ int handle_id(va_list i, int pl, int s, int lo, int hs, int w)
         {
                 j = (short)va_arg(i, int);
         }
+	else
+	{
+		j = va_arg(i, long);
+	}
 	if (j >= 0 && pl)
 	{
 		w--;
@@ -59,7 +63,6 @@ int handle_id(va_list i, int pl, int s, int lo, int hs, int w)
                                 		m += padding(w, strlen(k));
 					write(1, k, strlen(k));
 					free(k);
-  l = 0;
   return (m);
 }
 					
