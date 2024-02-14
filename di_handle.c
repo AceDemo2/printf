6e47010ecae148546dbcb94cb6e10658d7873def
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-02-14 20:15:49 +0100

    Update di_handle.c

--- di_handle.c
+++ di_handle.c
@@ -8,6 +8,12 @@ int handle_id(va_list i, int pl, int s, int lo, int hs, int w)
 	if (lo == 1)
         {
                 j = va_arg(i, long);
+		if (j < 0)
+		{
+			m += write(1, "-", 1);
+			j = -j;
+			w--;
+		}
         }
         else if (hs == 1)
         {
@@ -27,42 +33,35 @@ int handle_id(va_list i, int pl, int s, int lo, int hs, int w)
 		w--;
 		m += write(1, " ", 1);
 	}
-	else if (j < 0)
+	if (j == 0)
+		l++;
+	else
 	{
-		m += write(1, "-", 1);
-		j = -j;
-		w--;
+		o = j;
+		while (o != 0)
+		{
+			o = o / 10;
+			l++;
+		}
 	}
-					if (j == 0)
-						l++;
-					else
-					{
-						o = j;
-						while (o != 0)
-						{
-							o = o / 10;
-							l++;
-						}
-					}
-					k = malloc(sizeof(unsigned int ) * l + 2);
-					if (k == NULL)
-					{
-						va_end(i);
-						return (-1);
-					}
-					k[l] = '\0';
-					n = l - 1;
-					while (n >= 0)
-					{
-						k[n] = j % 10 + '0';
-						m++;
-						n--;
-						j /= 10;
-					}
-					if (w > l)
-                                		m += padding(w, strlen(k));
-					write(1, k, strlen(k));
-					free(k);
-  return (m);
+	k = malloc(sizeof(unsigned int ) * l + 2);
+	if (k == NULL)
+	{
+		va_end(i);
+		return (-1);
+	}
+	k[l] = '\0';
+	n = l - 1;
+	while (n >= 0)
+	{
+		k[n] = j % 10 + '0';
+		m++;
+		n--;
+		j /= 10;
+	}
+	if (w > l)
+        m += padding(w, strlen(k));
+	write(1, k, strlen(k));
+	free(k);
+	return (m);
 }
-					
