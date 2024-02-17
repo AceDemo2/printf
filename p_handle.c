35b6b0cb596d29c5590c4a9876fd6fd10b0ac0df
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-02-17 14:14:23 -0600

    Update and rename p_handle.ce to p_handle.c

--- p_handle.c
+++ p_handle.c
@@ -3,10 +3,12 @@
 int handle_p(va_list i, int w)
 {
 	int m = 0;
+	void *p;
 
+	p = va_arg(i, void *);
 	if (p == NULL)
 		m += write(1, "(nil)", 5);
 	else
-		m += handle((i, 16, 2, 0, 0, w);
+		m += handle(((unsigned long)p, 16, 2, 0, 0, w);
 	return (m);
 }
