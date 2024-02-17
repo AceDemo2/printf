61d85f6c95a91d0d496537fc4d7c7d762e24dda7
AceDemo2 <142119014+AceDemo2@users.noreply.github.com>
2024-02-17 10:52:02 -0600

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -96,7 +96,7 @@ int _printf(const char *format, ...)
                         else
                         {
 
-                                format -= rd - 4;
+                                format -= rd - 1;
                                 m += write(1, format, 1);
                         }
                         format++;
